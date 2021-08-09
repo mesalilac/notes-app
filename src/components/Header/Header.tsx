4c690553c596987123dd0059a6086a24caad6c30
senpai-10 <bmjfdrh@gmail.com>
2021-08-09 21:32:19 +0300

    header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+export default function Header({notes, handleAddNote, handleDeleteNote}: any) {
+  return (
+    <>
+      <div className="header">
+        <h1>Notes</h1>
+        <button className="toggle-button">toggle mode</button>
+      </div>
+    </>
+  );
+}
