bb06ec33a89984475350b74fbe53abd6ee153944
senpai-10 <bmjfdrh@gmail.com>
2021-08-09 21:57:23 +0300

    remove objects from notes useState

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,7 @@ import { nanoid } from "nanoid"
 */
 
 export default function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "this is the note number 1",
-      date: "9/8/2021"
-    },
-    {
-      id: nanoid(),
-      text: "this is the note number 2",
-      date: "10/8/2021"
-    },
-    {
-      id: nanoid(),
-      text: "this is the note number 3",
-      date: "11/8/2021"
-    },
-    {
-      id: nanoid(),
-      text: "this is the note number 4",
-      date: "12/8/2021"
-    }
-  ]);
+  const [notes, setNotes] = useState([]);
   const [searchText, setSearchText] = useState('')
 
   useEffect(() => {
