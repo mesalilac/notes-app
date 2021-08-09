35095469ea8a1353d3dac85284038db3dd21a964
senpai-10 <bmjfdrh@gmail.com>
2021-08-09 19:02:28 +0300

    import Noteslist

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import "./App.css";
+import NotesList from "./components/NotesList/NotesList";
 
 export default function App() {
   return (
-    <>
-      <p>hi</p>
-    </>
+    <div className="container">
+      <NotesList />
+    </div>
   );
 }
