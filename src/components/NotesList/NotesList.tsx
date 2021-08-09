6f71b123a50cc146ea2992fc615aafd3b61cc828
senpai-10 <bmjfdrh@gmail.com>
2021-08-09 21:02:18 +0300

    add note

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -1,12 +1,13 @@
+import AddNote from "../addNote/AddNote";
 import Note from "../note/Note";
 import "./NotesList.css";
 
-export default function NotesList({notes}: any) {
-  
+export default function NotesList({notes, handleAddNote, handleDeleteNote}: any) {
   return (
     <>
       <div className="notes-list">
-        {notes.map((note: any) => <Note key={note.id} text={note.text} date={note.date}/>)}
+        {notes.map((note: any) => <Note key={note.id} id={note.id} text={note.text} date={note.date} handleDeleteNote={handleDeleteNote}/>)}
+        <AddNote handleAddNote={handleAddNote} />
       </div>
     </>
   );
