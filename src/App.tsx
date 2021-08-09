cffe320d567ce944073d05317aa543bd0d8712b0
senpai-10 <bmjfdrh@gmail.com>
2021-08-09 19:33:16 +0300

    pass the notes array

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import NotesList from "./components/NotesList/NotesList";
 import "./app.css";
+import { useState } from "react";
+import { nanoid } from "nanoid" 
 
 export default function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "this is the note number 1",
+      date: "9/8/2021"
+    },
+    {
+      id: nanoid(),
+      text: "this is the note number 2",
+      date: "10/8/2021"
+    },
+    {
+      id: nanoid(),
+      text: "this is the note number 3",
+      date: "11/8/2021"
+    },
+    {
+      id: nanoid(),
+      text: "this is the note number 4",
+      date: "12/8/2021"
+    }
+  ]);
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes}/>
     </div>
   );
 }
