f5abf0245afe5fe38206eacd214ee544b1e5594a
senpai-10 <bmjfdrh@gmail.com>
2021-08-09 21:25:16 +0300

    search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,10 +1,16 @@
-import "./Search.css";
+import { MdSearch } from "react-icons/md";
+import "./Search.css"
 
-export default function Search() {
+export default function Search({handleSearchNote}: any) {
   return (
     <>
-      <div className="seach-bar">
-        
+      <div className="search">
+        <MdSearch className="search-icon" size="1.3em"/>
+        <input 
+          onChange={(event) => handleSearchNote(event.target.value)}
+          type="text" 
+          placeholder="type to search for a note"
+        />
       </div>
     </>
   );
